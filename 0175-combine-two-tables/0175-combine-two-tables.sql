b4bc9fd98048701dd11dfa6198ddc75039918125
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-17 11:10:37 +0530

    Time: 413 ms (28.11%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -4,3 +4,19 @@ SELECT firstname,lastname,city,state
 FROM Person
 LEFT JOIN Address
 USING (personID);
+
+
+#Different approaches
+/* 
+SELECT p.firstname,p.lastname,a.city,a.state
+FROM Person P
+LEFT JOIN Address P
+ON P.personID=A.personId;
+
+OR,
+
+SELECT p.firstname,p.lastname,a.city,a.state
+FROM Person P
+LEFT JOIN Address P
+Using(personId);
+*/
