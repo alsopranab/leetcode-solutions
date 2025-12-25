8473b0421befd989781049864c8374317c04a15a
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-25 17:54:27 +0530

    Time: 1221 ms (81.93%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -4,12 +4,17 @@
 
 #first thing first.
 
-/* using */ select s.name
+/* using */ select s.name       /* selecting the name from the salesperson table*/
 from salesperson s
-where s.sales_id not in
-            ( select sales_id
+where s.sales_id not in         /* using "NOT IN" in so that we can select the salesperson
+                                who actually sold the product and we can actually remove them during this process */
+         
+            ( select sales_id   /* selecting the company*/
             from Orders o
             inner join Company c
                 on o.com_id = c.com_id
-                where name = 'RED'
-            );
+                where name = 'RED')
+;
+
+# thats all.
+
