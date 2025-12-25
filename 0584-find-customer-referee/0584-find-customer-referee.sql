6242b3145f9f57f8df817fc391f3fd4e984daec4
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-25 17:02:52 +0530

    Time: 463 ms (43.2%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,5 @@
+# prb: find the customer - who is refereed by no one and also the who is referred by 2
+
 select name
 from customer c
-where referee_id is null or referee_id !=2;
+where referee_id  is null or referee_id  !=2;
