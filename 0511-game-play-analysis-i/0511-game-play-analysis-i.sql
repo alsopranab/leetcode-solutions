50f40e599da555b9682c0bd148659fab7e0eb84b
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-25 16:49:47 +0530

    Time: 445 ms (83.33%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
 
+
+/*# Prob: 
+-- find the first login means the minimum date
+-- then group the player id so that we can get the data /user
+*/
 select player_id, MIN(event_date) as first_login
 from Activity
 group by player_id;
