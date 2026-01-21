0e596343217a5e75af2f7104ffaa5dbf8d8c1e79
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-01-21 22:59:55 +0530

    Time: 627 ms (14.39%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
 
-# Write your MySQL query statement below
-
 SELECT employee_id
 FROM Employees
 WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
