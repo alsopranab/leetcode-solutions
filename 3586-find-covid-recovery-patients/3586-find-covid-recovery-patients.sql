dc9963f20e83e1b91078c9afdc7df541aa11932a
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-01-21 20:13:27 +0530

    Time: 616 ms (12.18%), Space: 0B (100%) - LeetHub

--- 3586-find-covid-recovery-patients/3586-find-covid-recovery-patients.sql
+++ 3586-find-covid-recovery-patients/3586-find-covid-recovery-patients.sql
@@ -8,22 +8,26 @@ Calculate recovery time using the difference between the positive and negative d
 Sort the output by recovery time and patient name.
 */
 
-SELECT 
-        p.patient_id,
-        p.patient_name,
-        p.age,
-        datediff 
-            (min(c2.test_date),min(test_date)) AS recovery_time
-
-        FROM covid_tests c1
-        JOIN covid_test c2
-            ON c1.patient_id = c2.patient_id
-            AND c1.test_date < c2.test_date
-            AND c1.result = 'Positive'
-            AND c2.result = 'Negative'
-
-        JOIN patient p
-        ON c1.patient_id = p.patient_id
-
-        GROUP BY p.patient_id, p.patient_name, p.age
-        ORDER BY recovery_time, p.patient_name
+SELECT
+    p.patient_id,
+    p.patient_name,
+    p.age,
+    DATEDIFF(
+        MIN(c2.test_date),
+        MIN(c1.test_date)
+    ) AS recovery_time
+FROM covid_tests c1
+JOIN covid_tests c2
+    ON c1.patient_id = c2.patient_id
+    AND c1.test_date < c2.test_date
+    AND c1.result = 'Positive'
+    AND c2.result = 'Negative'
+JOIN patients p
+    ON c1.patient_id = p.patient_id
+GROUP BY
+    p.patient_id,
+    p.patient_name,
+    p.age
+ORDER BY
+    recovery_time,
+    p.patient_name;
