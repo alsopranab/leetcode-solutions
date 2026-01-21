cb47cd7403a66e380a72655d67ade8e1dc262ebe
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-01-21 22:38:13 +0530

    Time: 1192 ms (32.44%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,12 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT DISTINCT
+SELECT
     p.product_id,
     p.product_name
-
 FROM product p
 JOIN sales s
     ON p.product_id = s.product_id
-    
-WHERE s.sale_date BETWEEN '2019-01-01' AND '2019-03-31';
+GROUP BY
+    p.product_id,
+    p.product_name
+HAVING
+    MIN(s.sale_date) >= '2019-01-01'
+    AND MAX(s.sale_date) <= '2019-03-31';
 
