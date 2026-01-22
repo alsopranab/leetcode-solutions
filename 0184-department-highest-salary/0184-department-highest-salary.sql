faf278f40670e5ffad103db4a2ee319ee95cd9b2
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-01-22 10:28:10 +0530

    Time: 986 ms (18.21%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,15 +1,20 @@
 # Write your MySQL query statement below
 
+/* Join the tables
+compare the salary against the dept*/
+
+-- Lets start
+
 Select
         d.name as Department,
         e.name as Employee,
         e.Salary
 From Employee e
-Join Department d
-On e.departmentId=d.id
+Join Department as d
+on e.departmentId=d.id
 
 Where e.salary = (
-    select max(e2.salary)
-    from employee e2
-    where e.departmentid=e2.departmentid
+    Select max(e2.salary)
+    From Employee e2
+    Where e.departmentid=e2.departmentId
 );
