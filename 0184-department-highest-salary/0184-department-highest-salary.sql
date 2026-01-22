0e4a2ea6539845af5c2fb6b3ae840c6520cc4bcf
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-01-22 10:33:23 +0530

    Time: 1039 ms (12.01%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -9,12 +9,12 @@ Select
         d.name as Department,
         e.name as Employee,
         e.Salary
-From Employee e
+From Employee e -- to check one employee
 Join Department as d
 on e.departmentId=d.id
 
 Where e.salary = (
     Select max(e2.salary)
-    From Employee e2
-    Where e.departmentid=e2.departmentId
+    From Employee e2 -- to comare all employees
+    Where e.departmentid=e2.departmentId -- but against their resp. department
 );
