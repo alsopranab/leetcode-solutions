73a2fd6a4a4a8b6cd8f8c6374bf3fc20b48a0554
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-02-12 07:23:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,54 +1,7 @@
-Select (
-    select distinct salary
-    from employee
-    order by salary desc
-    limit 1 offset 1
-) as SecondHighestSalary;
-
-
-# Write your MySQL query statement below
-
-# Easy Method - Distinct + Offset + Limit
-/*SELECT 
-     (SELECT DISTINCT Salary
-     FROM Employee
-     ORDER BY Salary
-     LIMIT 1 OFFSET 1)
-     AS SecondHighestSalary
-*/
-/* 
-### This method works with duplicates too but slightly lower than LIMIT func & cannot generate Nth Salary
-SELECT MAX(Salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary <(
-    Select Max(salary)
-    From Employee
-);
-
-/*
-### Window Function
-
 SELECT Salary AS SecondHighestSalary
 FROM (
     SELECT Salary,
            DENSE_RANK() OVER (ORDER BY Salary DESC) AS rnk
     FROM Employee
-) Ranked_salary
+) t
 WHERE rnk = 2;
-
-###using functions but won't work in leetcode
-
-Delimiter $$
-Create function getNthHighestSalary(2 int)
-Return INT
-Deterministic 
-Begin
-    Return (
-        Select Distinct Salary
-        From Employee
-        Order by Salary desc
-        limit 1 offset n-1
-    );
-end $$
-Delimiter ;
-*/
