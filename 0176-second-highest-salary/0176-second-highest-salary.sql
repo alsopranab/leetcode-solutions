578a786cbbf170e270c89296582ee0f794b716d5
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-03-09 01:01:00 +0530

    Time: 574 ms (5.15%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,6 @@
-Select (Select Distinct Salary
-From Employee
-Order by Salary Desc
-Limit 1 Offset 1) as SecondHighestSalary;
+Select (select distinct salary
+from Employee
+order by Salary desc
+limit 1 offset 1)
+as SecondHighestSalary;
+
